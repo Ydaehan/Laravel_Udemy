88fc1957f8977903f27b03f9c2bee40b9e4aaa97
유대한 <yoodaehan@yudaehan-ui-MacBookAir.local>
2025-03-21 01:54:31 +0900

    Laravel_Udemy_勉強

---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+
+@php
+$title = "Contact Page";
+@endphp
+
+<body>
+	<h2>{{ $title }}</h2>
+	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque obcaecati eum, doloribus quia commodi laborum
+	temporibus quae voluptatem ab? Ea dolorem corporis neque sapiente optio quis nihil, accusantium esse porro?
+</body>
+
+</html>
+
+{{-- @if ()
+@elseif()
+@endif
+
+@for ()
+
+@endfor
+
+@foreach ( as )
+
+@endforeach --}}
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,63 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
+
+// 一般的なRoute
+Route::get('/about', function () {
+	return "This is about page";
+})->name('about');
+
+// Route Parameters　{id} <-- これがパラメータ
+Route::get('/user/{id}/{slug}', function ($id, $slug) {
+	// このようにしてパラメータを取得できる
+	return "Say Hello! " . $id . "-" . $slug;
+})->name('user');
+
+Route::group(['prefix' => 'blog', 'as' => 'blog.'], function () {
+	Route::get('/create', function () {
+		return "Blog Create Page";
+	})->name('create');
+	Route::get('/edit', function () {
+		return "Blog Edit Page";
+	})->name('edit');
+	Route::get('/show', function () {
+		return "Blog Show Page";
+	})->name('show');
+});
+
+// Route Methods
+/**
+ * 1. GET // dataを取得する
+ * 2. POST // dataを送信する
+ * 3. PUT // dataを更新する -> IDとか全てのデータを送信する
+ * 4. PATCH // dataを更新する -> Idとか一部のデータを送信する
+ * 5. DELETE　// dataを削除する
+ * 
+ * Route::get('get-route', function() {
+ *  return "GET Route";
+ * })
+ * Route::post('post-route', function() {
+ *  return "GET Route";
+ * })
+ * Route::put('put-route', function() {
+ *  return "GET Route";
+ * })
+ * Route::patch('patch-route', function() {
+ *  return "GET Route";
+ * })
+ * Route::delete('delete-route', function() {
+ *  return "GET Route";
+ * })
+ * 
+ */
+
+// Fallback route
+Route::fallback(function () {
+	return "Ooops! we can't find the page you are looking for :(";
+});
+
+Route::get('/contact', function () {
+	return view('contact.index');
+})->name('contact');
